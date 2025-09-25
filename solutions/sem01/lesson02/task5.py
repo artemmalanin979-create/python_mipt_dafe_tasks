fc44ae2141ba377cb83d3a3cfb794289a248e69c
artem_malanin <malanin.aa@phystech.edu>
2025-09-26 00:32:20 +0300

    format tasks

--- solutions/sem01/lesson02/task5.py
+++ solutions/sem01/lesson02/task5.py
@@ -1,5 +1,5 @@
 def get_gcd(num1: int, num2: int) -> int:
     # ваш код
     while num2:
-        num1, num2 = num2, num1 % num2 
+        num1, num2 = num2, num1 % num2
     return num1
--- solutions/sem01/lesson02/task6.py
+++ solutions/sem01/lesson02/task6.py
@@ -1,10 +1,11 @@
 def prost(num):
     if num == 1:
-        return False 
-    for divisor in range(2, int(num**0.5) + 1): 
-        if num % divisor == 0: 
-            return False 
+        return False
+    for divisor in range(2, int(num**0.5) + 1):
+        if num % divisor == 0:
+            return False
     return True
+
 
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
@@ -12,10 +13,10 @@ def get_sum_of_prime_divisors(num: int) -> int:
     if prost(num):
         sum_of_divisors = num
     else:
-        for divisor in range(2, int(num ** 0.5) + 1):
+        for divisor in range(2, int(num**0.5) + 1):
             if num % divisor == 0:
                 if prost(divisor):
                     sum_of_divisors += divisor
                 if prost(num // divisor):
-                    sum_of_divisors += (num // divisor)
+                    sum_of_divisors += num // divisor
     return sum_of_divisors
