ec44b4f640e966647fbbece90630fd0e4b68f992
artem_malanin <malanin.aa@phystech.edu>
2025-10-07 23:51:35 +0300

    les02 task6 reformatted

--- solutions/sem01/lesson02/task6.py
+++ solutions/sem01/lesson02/task6.py
@@ -28,7 +28,8 @@ def get_sum_of_prime_divisors2(num: int) -> int:
     else:
         for divisor in range(2, int(num**0.5) + 1):
             if num % divisor == 0:
-                sum_of_divisors += prost(divisor) + prost(num // divisor) 
+                sum_of_divisors += prost(divisor) + prost(num // divisor)
     return sum_of_divisors
+
 
 print(get_sum_of_prime_divisors2(9))
