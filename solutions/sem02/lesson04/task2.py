980611e76cacadc1c9c8876f5d75b578e6fc242e
artem_malanin <malanin.aa@phystech.edu>
2026-03-13 12:44:29 +0300

    solution of dominant_color function

--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -6,5 +6,39 @@ def get_dominant_color_info(
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
     # ваш код
+    if threshold < 1:
+        raise ValueError
+    
+    color_groups = {}
+    
+    h, w = image.shape
+    total_pixels = h * w
 
-    return 0, 0
+    unique_values = np.unique(image)
+
+    sorted_values = sorted(unique_values)
+    
+    groups = []
+    current_group = [sorted_values[0]]
+    
+    for i in range(1, len(sorted_values)):
+        if sorted_values[i] - sorted_values[i-1] < threshold:
+            current_group.append(sorted_values[i])
+        else:
+            groups.append(current_group)
+            current_group = [sorted_values[i]]
+    groups.append(current_group)
+    
+    max_count = 0
+    best_group = None
+    
+    for group in groups:
+        count = sum(np.sum(image == val) for val in group)
+        if count > max_count:
+            max_count = count
+            best_group = group
+    
+    dominant_color = int(round(np.mean(best_group)))
+    percentage = (max_count / total_pixels) * 100
+    
+    return (dominant_color, percentage)
