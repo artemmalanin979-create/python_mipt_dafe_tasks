7ec4d9adc6f964e84bc37ea6b48c38c0db6622aa
artem_malanin <malanin.aa@phystech.edu>
2026-05-01 03:26:20 +0300

    after ruff

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
